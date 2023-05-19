1feed8f4944149ea99c770e5aaf0b6af9fa67115
Sean Nash <sean.c.nash@gmail.com>
2023-05-18 20:12:54 -0400

    Chapter 1

---
+++ chapter01/Ch01_01_bm.cpp
@@ -0,0 +1,111 @@
+//------------------------------------------------
+//               Ch01_01.cpp
+//------------------------------------------------
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include "Ch01_01.h"
+
+#include <benchmark/benchmark.h>
+
+#include <experimental/simd>
+
+namespace stdx = std::experimental;
+
+constexpr size_t n = 999999;
+
+alignas(stdx::memory_alignment_v<stdx::fixed_size_simd<float, 8>>) static float x[n];
+alignas(stdx::memory_alignment_v<stdx::fixed_size_simd<float, 8>>) static float y[n];
+alignas(stdx::memory_alignment_v<stdx::fixed_size_simd<float, 8>>) static float z1[n];
+alignas(stdx::memory_alignment_v<stdx::fixed_size_simd<float, 8>>) static float z2[n];
+alignas(stdx::memory_alignment_v<stdx::fixed_size_simd<float, 8>>) static float z3[n];
+
+void setup(const benchmark::State& state)
+//void setup()
+{
+    for (size_t i = 0; i < n; i++)
+    {
+        x[i] = i * 10.0f + 10.0f;
+        y[i] = i * 1000.0f + 1000.0f;
+        z1[i] = z2[i] = z3[i] = 0.0f;
+    }
+}
+
+BENCHMARK_MAIN();
+
+static void BM_cpp(benchmark::State& state)
+{
+    
+    for(auto _ : state) {
+        CalcZ_Cpp(z1, x, y, n);
+    }
+    
+}
+BENCHMARK(BM_cpp)->Setup(setup);
+
+static void BM_avx(benchmark::State& state)
+{
+    
+    for(auto _ : state) {
+        CalcZ_Iavx(z1, x, y, n);
+    }
+    
+}
+BENCHMARK(BM_avx)->Setup(setup);
+
+
+static void BM_asm(benchmark::State& state)
+{
+    
+    for(auto _ : state) {
+        CalcZ_Aavx(z1, x, y, n);
+    }
+    
+}
+BENCHMARK(BM_asm)->Setup(setup);
+
+
+static void BM_simd(benchmark::State& state)
+{
+    
+    for(auto _ : state) {
+        CalcZ_simd(z1, x, y, n);
+    }
+    
+}
+BENCHMARK(BM_simd)->Setup(setup);
+
+/*
+int main()
+{
+
+    // Exercise the SIMD calculating functions
+    (z1, x, y, n);
+    CalcZ_Iavx(z2, x, y, n);
+    CalcZ_Aavx(z3, x, y, n);
+
+    // Display the results
+    const char nl = '\n';
+    const size_t w = 10;
+    std::cout << std::fixed << std::setprecision(1);
+
+    std::cout << std::setw(w) << "i";
+    std::cout << std::setw(w) << "x";
+    std::cout << std::setw(w) << "y";
+    std::cout << std::setw(w) << "z1";
+    std::cout << std::setw(w) << "z2";
+    std::cout << std::setw(w) << "z3" << nl;
+    std::cout << std::string(60, '-') << nl;
+
+    for (size_t i = 0; i < n; i++)
+    {
+        std::cout << std::setw(w) << i;
+        std::cout << std::setw(w) << x[i];
+        std::cout << std::setw(w) << y[i];
+        std::cout << std::setw(w) << z1[i];
+        std::cout << std::setw(w) << z2[i];
+        std::cout << std::setw(w) << z3[i] << nl;
+    }
+}
+*/
